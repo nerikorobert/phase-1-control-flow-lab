5b5bbd944f98653cb57b101dc5881df0a24582b5
robert neriko <nerikorobert@gmail.com>
2023-07-11 14:39:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if(someValue <= 400){
+    return "This one is on me!";
+  }else if(someValue > 2000 && someValue <= 2500){
+    return "I will gladly take your thirty bucks.";
+
+ }else if(someValue > 2500){
+      return "No can do.";
+
+}
+}
+function ternaryCheckCity(cityName){
+ if(cityName === "NYC"){
+  return "Ok, sounds good.";
+ }else{
+      return "No go.";
+ }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(joyName){
+  if(joyName === "generous"){
+    return "Thank you so much.";
+  }else if(joyName === "not as generous"){
+    return "Thank you.";
+  }else{
+    return "Bye.";
+  }
 }
